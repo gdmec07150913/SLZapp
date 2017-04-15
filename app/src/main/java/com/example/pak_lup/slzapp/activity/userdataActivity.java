6b3591d6b3b238f5f89f8e4b41cf6165d4264621
gdmec07150913 <408594990@qq.com>
2017-04-15 20:53:37 +0800

    更新初步框架,更改用户资料

---
+++ app/src/main/java/com/example/pak_lup/slzapp/activity/userdataActivity.java
@@ -0,0 +1,43 @@
+package com.example.pak_lup.slzapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.pak_lup.slzapp.R;
+
+public class userdataActivity extends AppCompatActivity {
+    Toolbar toobar;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userdata);
+        findView();
+
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//返回按钮的显示与实现
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void findView() {
+        toobar= (Toolbar) findViewById(R.id.toolbar);
+        tv= (TextView) findViewById(R.id.tv_title);
+        tv.setText("用户资料");
+        toobar.setTitle("");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
